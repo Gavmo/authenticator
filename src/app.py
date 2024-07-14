51221aac36679135e6b3fec1224fd53c1bd6589f
Gavmo <1gavmo@gmail.com>
2024-07-14 10:00:27 +1000

    traefik configuration test

--- src/app.py
+++ src/app.py
@@ -14,6 +14,13 @@ def rand_string(char_len):
     return return_str
 
 
+def router_config(service, host, token):
+    return f"""http:\n""" \
+           f"""  routers:\n""" \
+           f"""    {service}:\n""" \
+           f"""      rule=Host(`{host}`)&&Header(`Cookie`, `SESSION={token}`)\n"""
+
+
 @app.route("/", methods=['GET', 'POST'])
 def login_page():
     if request.method == 'POST':
@@ -29,5 +36,22 @@ def login_page():
                         return resp
         return request.form
     return render_template(f"login.html", )
+
+
+@app.route("/api/traefik_conf")
+def traefik_conf():
+    """Quick smash together for POC testing"""
+    service_dict = dict()
+    return_str = ""
+    with open("../data/creds.csv", "r") as creds_file, open("../data/service_to_url.csv", 'r') as service_file:
+        for record in csv.reader(service_file):
+            service_dict[record[0]] = record[1]
+        for record in csv.reader(creds_file):
+            return_str += router_config(service_dict[record[3]], record[3], record[2])
+    return return_str
+
+    # consult a table of data
+    # return a JSON containing the routing rules
+
 
 
